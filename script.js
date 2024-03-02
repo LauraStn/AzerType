e2f05115e2a3d13d639ad5d34e833a27438076e5
Laura <laura.santin73@gmail.com>
2024-03-02 16:38:32 +0100

    remove test page

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-
-/*
-créer un bouton "commencer"
-quand on clique sur commencer la boucle se met en route
-*/ 
-let listeMots = ["Raiden", "Nilou", "Ganyu", "Zhongli", "Ayaka", "Alhaitham", "Yoimiya", "Nahida", "Furina"]
+let wordList = ["Raiden", "Nilou", "Ganyu", "Zhongli", "Ayaka", "Alhaitham", "Yoimiya", "Nahida", "Furina"]
 let score = 0
 let wordHint = document.getElementById("wordHint")
-let rand = Math.floor(Math.random() * listeMots.length)
-let wordRandom = listeMots[rand]
+let rand = Math.floor(Math.random() * wordList.length)
+let wordRandom = wordList[rand]
 let userScore = document.getElementById("userScore")
 
-// Fonction pour choisir un nouveau mot aléatoire
+
 function newWord() {
-    let rand = Math.floor(Math.random() * listeMots.length);
-    return listeMots[rand];
+    let rand = Math.floor(Math.random() * wordList.length);
+    return wordList[rand];
 }
 
 function updateScore() {
@@ -30,12 +25,9 @@ document.querySelector("form").addEventListener("submit", (e) =>{
             score ++    
             userScore.innerText = `Votre Score: ${score}`
             window.alert("Bravo!")
-            form.reset() //Réinitialiser le formulaire
-            wordRandom = newWord() //Génère un nouveau mot aléatoire
+            form.reset()
+            wordRandom = newWord()
             wordHint.innerHTML = `Entrez le mot suivant: ${wordRandom}`
-    //    for (score = 0; score <listeMots.length; score++){
-    //     wordHint.innerText = `Entrez le mot suivant: ${wordRandom}`
-    //    }    
     }else {
             window.alert("Réessayez")
             form.reset()
@@ -44,10 +36,9 @@ document.querySelector("form").addEventListener("submit", (e) =>{
 })
 
 document.querySelector("#start").addEventListener("click", (e) => {
-    score = 0 // Réinitialiser le score
+    score = 0 //
     updateScore()
     window.alert("C'est parti!")
-    wordRandom = newWord(); // Choisir un nouveau mot aléatoire
+    wordRandom = newWord();
     wordHint.innerText = `Entrez le mot suivant: ${wordRandom}`
-    // document.querySelector("form").reset() // Réinitialiser le formulaire pour vider le champ
 })
